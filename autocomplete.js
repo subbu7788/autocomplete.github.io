32a18739adbc8628ea1c05d6c55275389481c6ff
subbu7788 <subbu7788@live.in>
2020-03-12 23:58:13 +0530

    Update autocomplete.js

--- autocomplete.js
+++ autocomplete.js
@@ -7,7 +7,7 @@ if(ele.type ==='email' || ele.type ==='text' || ele.type ==='image' || ele.type 
         var dt = new Date();    
         var ids = "id"+dt.getTime()+ "_"+ Math.floor(Math.random()*1000);       
       ele.style.outline="#af4a00 solid 5px"    ;                        
-ele.insertAdjacentHTML("beforeBegin"," <br><div style='color:#af4a00;position:relative;bottom:5px;font-weight:700;background-color: white'> autocomplete attribute not provided   <button style='color: rgba(0,0,0,.8);border: 1px solid #999;background-color: #e6e6e6;border-radius: 2px;' onclick=\"copytext('"+ids +"');\" > Copy Code</button></div>  ");
+ele.insertAdjacentHTML("beforeBegin"," <br><div style='color:#af4a00;position:relative;bottom:5px;font-weight:700;background-color: white;padding: 5px; margin: 5px'> autocomplete attribute not provided   <button style='color: rgba(0,0,0,.8);border: 1px solid #999;background-color: #e6e6e6;border-radius: 2px;' onclick=\"copytext('"+ids +"');\" > Copy Code</button></div>  ");
 ele.insertAdjacentHTML("afterEnd"," <br>  <textarea  type='hidden' style='display:none' id=  '"+ids +"'>" + ele.outerHTML+"</textarea> ");
     }
     else{
@@ -15,7 +15,7 @@ ele.insertAdjacentHTML("afterEnd"," <br>  <textarea  type='hidden' style='displa
         var rn = Math.random() * 1000;
         var ids = "id"+dt.getTime()+ "_"+Math.floor(Math.random()*1000);  
         ele.style.outline="#af4a00 solid 5px" ;                           
-ele.insertAdjacentHTML("beforeBegin", " <br><div style='color:#af4a00;position:relative;bottom:5px;font-weight:700;background-color: white'> autocomplete='"+ ele.autocomplete+"'  <button style='color: rgba(0,0,0,.8);border: 1px solid #999;background-color: #e6e6e6;border-radius: 2px;' onclick=\"copytext('"+ids +"');\" > Copy Code</button></div> ");
+ele.insertAdjacentHTML("beforeBegin", " <br><div style='color:#af4a00;position:relative;bottom:5px;font-weight:700;background-color: white;padding: 5px; margin: 5px'> autocomplete='"+ ele.autocomplete+"'  <button style='color: rgba(0,0,0,.8);border: 1px solid #999;background-color: #e6e6e6;border-radius: 2px;' onclick=\"copytext('"+ids +"');\" > Copy Code</button></div> ");
 ele.insertAdjacentHTML("afterEnd"," <br>  <textarea  type='hidden' style='display:none' id=  '"+ids +"'>" + ele.outerHTML+"</textarea> ");
     }
     }
